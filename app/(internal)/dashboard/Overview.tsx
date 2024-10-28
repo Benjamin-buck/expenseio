18737fb1413ddcd78ff5d9fc39880fae3a73621a
Benjamin-buck <164558323+Benjamin-buck@users.noreply.github.com>
2024-10-28 14:05:04 -0400

    Adds responsive grid layout.

---
+++ app/(internal)/dashboard/Overview.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Overview = () => {
+  return (
+    <div className="card">
+      <h2 className="card_heading">Monthly Overivew</h2>
+      <p className="card_desc">A summary of your expenses this month.</p>
+    </div>
+  );
+};
+
+export default Overview;
