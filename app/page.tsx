1f322138314075b24e41f74f0959bd9af5e65a2b
Benjamin-buck <164558323+Benjamin-buck@users.noreply.github.com>
2024-10-30 17:28:45 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1 className="font-instrumentSans font-semibold text-4xl">Home</h1>
+      <h1 className="font-instrumentSans font-semibold text-4xl">Home 1</h1>
     </div>
   );
 }
