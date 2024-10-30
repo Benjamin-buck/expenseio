76f80649b7b8684df44c07290988fe963105151a
Benjamin-buck <164558323+Benjamin-buck@users.noreply.github.com>
2024-10-30 11:47:44 -0400

    starts category mapping

--- app/dashboard/expenses/CategoriesTable.tsx
+++ app/dashboard/expenses/CategoriesTable.tsx
@@ -9,9 +9,55 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import prisma from "@/prisma/client";
+import { Expense } from "@prisma/client";
+import {
+  ReactElement,
+  JSXElementConstructor,
+  ReactNode,
+  ReactPortal,
+  AwaitedReactNode,
+} from "react";
 
-const CategoriesTable = async () => {
-  const categories = await prisma.category.findMany();
+const CategoriesTable = async ({ expenses }: { expense: Expense[] }) => {
+  const categories = await prisma.category.findMany({});
+
+  // Logic
+  // Create an empy array for the expense category objects
+  const expenseCategories:
+    | { id: any; category: any; price: any }[]
+    | (
+        | string
+        | number
+        | bigint
+        | boolean
+        | ReactElement<any, string | JSXElementConstructor<any>>
+        | Iterable<ReactNode>
+        | ReactPortal
+        | Promise<AwaitedReactNode>
+        | null
+        | undefined
+      )[] = [];
+  // Run a loop through all expense arrays
+
+  for (let i = 0; i < expenses.length; i++) {
+    const category = {
+      id: expenses[i].categoryId,
+      category: expenses[i].category.title,
+      price: expenses[i].price,
+    };
+    if (expenseCategories[category.id - 1]) {
+      expenseCategories[category.id - 1].price += category.price;
+      // expenseCategories[category.category].price = +category.price;
+    } else {
+      expenseCategories.push(category);
+    }
+  }
+
+  console.log(expenseCategories);
+  console.log(expenseCategories[1].category);
+
+  // If the category has already been added to the expenseCaterogires array, add to the total.
+  // Else, add the new object to the array.
 
   return (
     <div className="card">
@@ -30,11 +76,11 @@ const CategoriesTable = async () => {
           </TableRow>
         </TableHeader>
         <TableBody>
-          {categories.map((category) => (
-            <TableRow key={category.id}>
-              <TableCell>{category.title}</TableCell>
+          {categories.map(({ id, label }) => (
+            <TableRow key={id}>
+              <TableCell>{expenseCategories[0].category}</TableCell>
               <TableCell className="text-right">
-                <span className="font-bold text-green-500">20$</span> of{" "}
+                <span className="font-bold text-green-500"></span> of{" "}
                 <span className="font-bold text-green-500">200$</span>
               </TableCell>
             </TableRow>
--- app/dashboard/expenses/page.tsx
+++ app/dashboard/expenses/page.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import ExpensesTable from "./ExpensesTable";
 import TotalExpenses from "./TotalExpenses";
 import prisma from "@/prisma/client";
+import CategoriesTable from "./CategoriesTable";
 const page = async () => {
   const expenses = await prisma.expense.findMany({
     include: {
@@ -16,7 +17,7 @@ const page = async () => {
       <div className="grid grid-cols-8 w-full gap-4">
         <div className="col-span-2">
           <TotalExpenses expenses={expenses} />
-          {/* <CategoriesTable /> */}
+          <CategoriesTable expenses={expenses} />
           {/* <SavingsTable /> */}
         </div>
         <div className="col-span-6">
